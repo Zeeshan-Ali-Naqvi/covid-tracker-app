436d7c3102532dd758b9a0b07215d14c3f10b364
zalinaqv <zalinaqv@ZALINAQV-6J3CZ.cisco.com>
2022-03-29 06:41:07 +0530

    Refactoring Code

--- covid-tracker-app/src/main/java/io/kugelblitz/coronavirustracker/CovidTrackerAppApplication.java
+++ covid-tracker-app/src/main/java/io/kugelblitz/coronavirustracker/CovidTrackerAppApplication.java
@@ -1,4 +1,4 @@
-package io.kugelblitz;
+package io.kugelblitz.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ covid-tracker-app/src/main/java/io/kugelblitz/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,5 @@
+package io.kugelblitz.coronavirustracker.services;
+
+public class CoronaVirusDataService {
+
+}
